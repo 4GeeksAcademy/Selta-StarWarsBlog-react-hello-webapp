655383deaa98d8d4da4ba75687d78fac39d7f72b
Selta7 <132694122+Selta7@users.noreply.github.com>
2023-12-04 18:32:24 +0000

    Updated flux for details

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,68 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			people: [],
+			planets: [],
+			starships:[]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
+			getPeople: async () => {
 				const store = getStore();
+				const textResponse = await fetch ("https.//www.swapi.tech/api/people");
+				const jsonResponse = await textResponse.json ();
+				setStore({...store, people: jsonResponse.results })
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			},
+			getPlanets: async () => {
+				const store = getStore();
+				await fetch ("https.//www.swapi.tech/api/planets");
+				const jsonResponse = await textResponse.json ();
+				setStore({...store, planets: jsonResponse.results })
+			},
+			
+			getStarships: async () => {
+				const store = getStore();
+				await fetch ("https.//www.swapi.tech/api/starships");
+				const jsonResponse = await textResponse.json ();
+				setStore({...store, starships: jsonResponse.results })
+			},	
+			
+			
+			getPersonDetails: async () => {
+				const store = getStore();
+//efficiently handle multiple asynchronous operations promise
+								const newPersonDetails = await Promise.all (store.people.map( async (person)=> {
+									const textResponse = await fetch (person.url);
+									const jsonResponse = await textResponse.json ();
+									return {...person, details: jsonResponse.results};
+								}));
+				
+				setStore({...store, peopl:newPersonDetails});
+			
+				
+			},
+			getPlanet: async () => {
+				const store = getStore();
+				
+								const newPlanetDetails = await Promise.all (store.people.map( async (person)=> {
+									const textResponse = await fetch (person.url);
+									const jsonResponse = await textResponse.json ();
+									return {...person, details: jsonResponse.results};
+								}));
+								
+								setStore({...store, peopl:newPlanetDetails});
+			},
+			getStarship: async () => {
+				const store = getStore();
+								
+								const newPlanetDetails = await Promise.all (store.people.map( async (person)=> {
+									const textResponse = await fetch (person.url);
+									const jsonResponse = await textResponse.json ();
+									return {...person, details: jsonResponse.results};
+								}));
+								
+								setStore({...store, peopl:newPlanetDetails});
+			},
 		}
 	};
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,41 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext, useEffect } from "react";
 import "../../styles/home.css";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
+export const Home = () => { 
+
+	const {store, actions} = useContext (Context);
+	const [isLoading, setIsLoading] = useState (false);
+	
+	const handleInitialFetch = async () => {
+		await actions.getPeople();
+		await actions.getPlanets();
+		await actions.getStarships();
+	}
+	useEffect (() => {
+		handleInitialFetch();
+
+	}, []);
+
+
+	return (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		<h1>Welcome to the StarWars Blogpedia </h1>
+		
+		<div>
+			<h2>People:</h2>
+				{store.people.map((person) => <p key={person.uid}>{person.name} </p>)}
+		</div>
+		<div>
+			<h2>Planets:</h2>
+				{store.planets.map((planets) => <p key={planets.uid}>{planets.name} </p>)}
+		</div>
+		<div>
+			<h2>Starships: </h2>
+				{store.starships.map((starships) => <p key={starships.uid}>{starships.name} </p>)}
+		</div>
+
+	
 	</div>
 );
+};
